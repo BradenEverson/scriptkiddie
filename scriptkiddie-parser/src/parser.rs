106d7b8963c72626cfe3ed57551c7f7b127d858e
Braden Everson <bradeneverson@gmail.com>
2024-10-14 09:59:12 -0500

    Cargo fmt

--- scriptkiddie-parser/src/parser.rs
+++ scriptkiddie-parser/src/parser.rs
@@ -4,5 +4,6 @@ use scriptkiddie_lexer::lexer::Lexer;
 
 /// A parser that holds onto a mutable context of a Lexer
 pub struct Parser<'lex> {
-    lexer: &'lex mut Lexer
+    /// The internal lexer session
+    lexer: &'lex mut Lexer,
 }
