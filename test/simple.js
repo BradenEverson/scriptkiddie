ecc68271a979bf85334f64c9d47621aff74cb68e
BradenEverson <bradeneverson@gmail.com>
2024-10-14 08:36:40 -0500

    Add comments to js test

--- test/simple.js
+++ test/simple.js
@@ -1,4 +1,10 @@
+/** This is a huge
+  * multiline comment
+  * it will be completely ignored
+  */
+
 let a = 100;
+// This is a single line comment that should be skipped
 let b = 20;
 
 let c = a + b;
